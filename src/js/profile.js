e594c2df20ad3d2f4ab4c1f147585046c1e093fb
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-06-25 12:12:51 -0500

    feat: user can delete account

--- src/js/profile.js
+++ src/js/profile.js
@@ -6,7 +6,26 @@ form.email.value = user.email;
 form.firstname.value = user.firstName;
 form.lastname.value = user.lastName;
 
+form.addEventListener('submit', (e) => e.preventDefault());
+
 form.edit.addEventListener('click', (e) => {
   e.preventDefault();
   window.location.replace('edit-profile.html');
+});
+
+form.delete.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetch(`http://localhost:3000/users/${user.id}`, {
+    method: 'DELETE',
+    headers: { 'Authorization': `Token token=${user.token}`},
+  })
+    .then((res) => {
+      if (res.ok) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        window.location.replace('login.html');
+      } else {
+        console.error(res);
+      }
+    });
 });
