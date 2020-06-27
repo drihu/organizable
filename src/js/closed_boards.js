6dd889199ef619abdf79d8c5b94383b148fc1ae2
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-06-26 19:15:14 -0500

    feat: user can delete permanently boards

--- src/js/closed_boards.js
+++ src/js/closed_boards.js
@@ -7,3 +7,62 @@ for (let i = 0; i < board.length; i++) {
     board[i].classList.add('hidden');
   });
 }
+
+function createBoard(board) {
+  const div = document.createElement('div');
+  div.classList.add('board');
+  div.style.background = board.color;
+  div.innerHTML = `
+    <p class="board__content">${board.name}</p>
+    <div class= "board__options ">
+      <i class="fa fa-trash-o"></i>
+      <i class="fa fa-undo"></i>
+    </div>
+  `;
+
+  const trashButton = div.querySelector('.fa-trash-o');
+  const undoButton = div.querySelector('.fa-undo');
+
+  trashButton.addEventListener('click', () => {
+    fetch(`http://localhost:3000/boards/${board.id}`, {
+      method: 'DELETE',
+      headers: { 'Authorization': `Token token=${localStorage.token}` },
+    })
+      .then((res) => {
+        if (res.ok) div.remove();
+        else console.error(res);
+      })
+  });
+
+  undoButton.addEventListener('click', () => {
+    fetch(`http://localhost:3000/boards/${board.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token token=${localStorage.token}`,
+      },
+      body: JSON.stringify({
+        board: { closed: false },
+      }),
+    })
+      .then((res) => {
+        if (res.ok) div.remove();
+        else console.error(res);
+      })
+  });
+
+  return div;
+}
+
+fetch("http://localhost:3000/boards", {
+  method: 'GET',
+  headers: { 'Authorization': `Token token=${localStorage.token}` }
+})
+  .then((res) => res.json())
+  .then((boards) => boards.filter((board) => board.closed))
+  .then((closedBoards) => {
+    closedBoards.forEach((board) => {
+      console.log(board);
+      wrapper.append(createBoard(board));
+    });
+  });
